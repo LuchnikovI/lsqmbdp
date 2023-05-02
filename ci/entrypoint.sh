13da12d4d9e6f26f5dbabdde5a47de1f5379720f
LuchnikovI <luchnikovilya@gmail.com>
2023-05-03 02:13:41 +0300

    entrypoint inside the container
    This script will be called first, when one starts a container

---
+++ ci/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+case $1 in
+
+  --test)
+        python3.10 -m pytest
+    ;;
+  --typecheck)
+        python3.10 -m mypy "${script_dir}/../src"
+    ;;
+  --lint)
+        pylint "${script_dir}/../src"
+    ;;
+  *)
+        echo "Unknown option: '$1'"
+        echo "Usage: $(basename $0) (--test|--typecheck)"
+        echo "More options will be implemented later"
+        exit 1
+    ;;
+esac
