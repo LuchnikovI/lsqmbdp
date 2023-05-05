188af3fe349b3d20e92fe0d152b12e2782b6ad33
LuchnikovI <luchnikovilya@gmail.com>
2023-05-05 19:32:56 +0300

    samplers, its utils and tests

--- src/sampler.py
+++ src/sampler.py
@@ -1,10 +1,15 @@
 """Sampler"""
 
+from functools import partial
 from typing import List
-from jax import Array
+from jax import Array, vmap
+from jax.random import KeyArray, split, categorical
 import jax.numpy as jnp
 from im import InfluenceMatrix
 from constants import projs
+from sampler_utils import _build_left_states, _push_to_left
+
+REGULARIZER = 1e-6
 
 Sampler = List[Array]
 
@@ -25,3 +30,48 @@ def im2sampler(
         return ker
     sampler = [translate_ker(ker) for ker in influence_matrix]
     return sampler
+
+
+@partial(vmap, in_axes=(0, None, 0))
+def _gen_samples(
+        subkey: KeyArray,
+        sampler: Sampler,
+        indices: Array,
+) -> Array:
+    left_states = _build_left_states(sampler, indices)
+    size = len(sampler)
+    right_state = jnp.ones((1,))
+    subkeys = split(subkey, size)
+    idx = size - 1
+    samples = jnp.zeros((size,))
+    while len(left_states) != 0:
+        left_state = left_states.pop()
+        ker = sampler[idx]
+        ker = ker[:, :, indices[idx]]
+        ker = jnp.tensordot(left_state, ker, axes=1)
+        ker = jnp.tensordot(ker, right_state, axes=1)
+        sample = categorical(subkeys[idx], jnp.log(ker.real + REGULARIZER), shape=(1,))
+        samples = samples.at[idx].set(sample[0])
+        right_state = _push_to_left(right_state, sampler[idx], indices[idx], sample[0])
+        norm = jnp.linalg.norm(right_state)
+        right_state /= norm
+        idx -= 1
+    return samples
+
+
+def gen_samples(
+        subkey: KeyArray,
+        sampler: Sampler,
+        indices: Array,
+) -> Array:
+    """Produces samples from a sampler.
+    Args:
+        subkey: jax random seed
+        sampler: Sampler
+        indices: a matrix with indices each of that runs from [0 to 16)
+            indicating the number of measurement basis
+    Returns: set of sampler"""
+
+    size = indices.shape[0]
+    subkey = split(subkey, size)
+    return _gen_samples(subkey, sampler, indices)
--- src/test_sampler.py
+++ src/test_sampler.py
@@ -3,11 +3,15 @@
 import pytest
 import jax.numpy as jnp
 from jax.random import PRNGKey, KeyArray, split, categorical
-from sampler import im2sampler
+from sampler import im2sampler, _gen_samples
 from im import random_im
 from mpa import mpa_sum, mpa2tensor
+from sampler_utils import (
+    _get_solid_probability,
+    _sample_from_solid_probability,
+)
 
-KEY = PRNGKey(42)
+KEY = PRNGKey(47)
 
 ACC = 1e-5
 
@@ -35,3 +39,26 @@ def test_im2sampler(
     assert (jnp.abs(sum_val - 1.) < ACC).all()
     arr = mpa2tensor(sampler).reshape((-1,))
     assert (arr >= -ACC).all()
+
+
+@pytest.mark.parametrize("subkey", split(KEY, 2))
+@pytest.mark.parametrize("time_steps", [1, 5])
+@pytest.mark.parametrize("local_choi_rank", [2, 4])
+@pytest.mark.parametrize("sqrt_bond_dim", [1, 5])
+def test_sampler(
+        subkey: KeyArray,
+        time_steps: int,
+        local_choi_rank: int,
+        sqrt_bond_dim: int,
+):
+    """Tests sampler comparing the MPS based version with the explicit one."""
+    _, subkey = split(subkey)
+    sampler = im2sampler(random_im(subkey, time_steps, local_choi_rank, sqrt_bond_dim))
+    for _ in range(10):
+        _, subkey = split(subkey)
+        index = categorical(subkey, jnp.ones((16,)), shape=(time_steps,))
+        _, subkey = split(subkey)
+        probability = _get_solid_probability(sampler, index)
+        solid_sample = _sample_from_solid_probability(subkey, probability)
+        sample = _gen_samples(subkey[jnp.newaxis], sampler, index[jnp.newaxis])
+        assert (sample[0] == solid_sample).all()
