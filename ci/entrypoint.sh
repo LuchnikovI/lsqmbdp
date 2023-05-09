299d375100e1e5367f7dfc0ba2114e01d7660a5c
LuchnikovI <luchnikovilya@gmail.com>
2023-05-09 08:36:34 +0300

    update th entrypoint

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -5,6 +5,7 @@ script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 export SQ_BOND_DIM=${SQ_BOND_DIM:-"8"}
 export TIME_STEPS=${TIME_STEPS:-"70"}
 export SAMPLES_NUMBER=${SAMPLES_NUMBER:-"1000"}
+export LOCAL_CHOI_RANK=${LOCAL_CHOI_RANK:-"2"}
 export SEED=${SEED:-"42"}
 
 case $1 in
@@ -13,17 +14,25 @@ case $1 in
         python3.10 -m pytest
     ;;
   --typecheck)
-        python3.10 -m mypy "${script_dir}/../src"
+        python3.10 -m mypy --exclude /qgoptax/ "${script_dir}/../src"
     ;;
   --lint)
-        pylint "${script_dir}/../src"
+        pylint --ignore-paths="${script_dir}/../src/qgoptax" "${script_dir}/../src"
     ;;
   --bench)
         "${script_dir}/../src/benchmarks.py"
     ;;
+  --gen_rand_im)
+        shift
+        "${script_dir}/../src/random_im.py" "$@"
+    ;;
+  --train_im)
+        shift
+        "${script_dir}/../src/train_im.py" "$@"
+    ;;
   *)
         echo "Unknown option: '$1'"
-        echo "Usage: $(basename $0) (--test|--typecheck|--lint|--bench)"
+        echo "Usage: $(basename $0) (--test|--typecheck|--lint|--bench|--gen_rand_im [options]|--train_im [options])"
         echo "More options will be implemented later"
         exit 1
     ;;
