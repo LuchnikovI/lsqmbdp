373332ac02d15fe2cd01405d4093eb491a960e9e
LuchnikovI <luchnikovilya@gmail.com>
2023-05-10 18:07:07 +0300

    random_im refactoring

--- src/random_im.py
+++ src/random_im.py
@@ -4,27 +4,31 @@ import sys
 import os
 import h5py # type: ignore
 from argparse import ArgumentParser
+import jax.numpy as jnp
 from im import random_im
 from jax.random import split, PRNGKey
 
-def main(argv):
-    script_path = os.path.dirname(argv[0])
-    sq_bond_dim = int(os.environ.get("SQ_BOND_DIM"))
-    time_steps = int(os.environ.get("TIME_STEPS"))
-    local_choi_rank = int(os.environ.get("LOCAL_CHOI_RANK"))
-    seed = int(os.environ.get("SEED"))
+
+SQ_BOND_DIM = int(str(os.environ.get("SQ_BOND_DIM")))
+TIME_STEPS = int(str(os.environ.get("TIME_STEPS")))
+LOCAL_CHOI_RANK = int(str(os.environ.get("LOCAL_CHOI_RANK")))
+SEED = int(str(os.environ.get("SEED")))
+SCRIPT_PATH = os.path.dirname(sys.argv[0])
+
+
+def main():
     parser = ArgumentParser()
     parser.add_argument('-n', '--name', default = "random_im")
     args = parser.parse_args()
     # -----------------------------------------------------------------------------
-    key = PRNGKey(seed)
+    key = PRNGKey(SEED)
     _, subkey = split(key)
-    infuence_matrix = random_im(subkey, time_steps, local_choi_rank, sq_bond_dim)
-    hf = h5py.File(script_path + "/../shared_dir/" + args.name, 'w')
+    influence_matrix = random_im(subkey, TIME_STEPS, LOCAL_CHOI_RANK, SQ_BOND_DIM)
+    hf = h5py.File(SCRIPT_PATH + "/../shared_dir/" + args.name, 'w')
     group = hf.create_group("im")
-    for i, ker in enumerate(infuence_matrix):
+    for i, ker in enumerate(influence_matrix):
         group.create_dataset(str(i), data=ker)
     hf.close()
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
